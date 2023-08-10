3f50fb78135fc9e9a85e6223e2783c77b5d5b634
walidlatif <walidlatif45@gmail.com>
2023-08-10 05:49:19 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'),ord('z') + 1):
-    if chr(i) not in ['q','e']:
+for i in range(ord('a'), ord('z') + 1):
+sh: 1: q: not found
         print("{0}".format(chr(i)), end='')
