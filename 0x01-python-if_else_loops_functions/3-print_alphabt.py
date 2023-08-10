db5d1d0a7ef6f5d4ee24626ccb5e8d16d9b32c14
walidlatif <walidlatif45@gmail.com>
2023-08-10 05:48:06 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+for i in range(ord('a'),ord('z') + 1):
+    if chr(i) not in ['q','e']:
+        print("{0}".format(chr(i)), end='')
