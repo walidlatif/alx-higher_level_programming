33ecc21e7b360e69c5920e23c4aa940653f35cd9
walidlatif <walidlatif45@gmail.com>
2023-08-10 13:24:06 -0700

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(0,100):
+    print("{:02}".format(int(i)), end='')
     if i != 99:
-        print("{:02}".format(i), end=", ")
-    else:
-        print("{}".format(i))
+        print(", ", end='')
