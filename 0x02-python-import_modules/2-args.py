43d3b426e2dc70346c2a8cce19e22ad7925e8e2e
walidlatif <walidlatif45@gmail.com>
2023-08-11 07:44:38 -0700

    delete.0-import...

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 1:
+    len = len(argv)
+    if len == 1:
         print("0 arguments.")
-    elif len(argv) > 1:
-        print("{:d} argument{:s}".format(len(argv) - 1, "s:" if len(argv) > 2 else ":"))
-        for i in range(1, len(argv)):
+    elif len > 1:
+        print("{:d} argument{:s}".format(len - 1, "s:" if len > 2 else ":"))
+        for i in range(1, len):
             print("{:d}: {:s}".format(i, argv[i]))
