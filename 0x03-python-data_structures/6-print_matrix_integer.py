452137112188d81ed25d5c48abec1a3acec2c3e2
walidlatif <walidlatif45@gmail.com>
2023-08-14 10:42:51 -0700

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             print("{:d}".format(element),
-                    end="" if row[len(row)-1] == element else " ")
+                  end="" if row[len(row)-1] == element else " ")
         print("".format())
