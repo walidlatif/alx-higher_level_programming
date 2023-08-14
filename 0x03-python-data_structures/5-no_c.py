be7eec7605bc664454df8f945924a02353de8051
walidlatif <walidlatif45@gmail.com>
2023-08-14 09:24:08 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char not in ['c','C']:
+        if char not in ['c', 'C']:
             new_string += char
     return new_string
