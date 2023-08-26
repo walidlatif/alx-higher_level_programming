c3587bfef3c4eb8d4ca8e9db5f168b78ac9be6df
walidlatif <walidlatif45@gmail.com>
2023-08-26 09:41:02 -0700

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end='')
-    if i != 99:
-        print(", ", end='')
+for number in range(0, 100):
+    separator = ", "
+    if number == 99:
+        separator = "\n"
+    print("{:02d}".format(number), end=separator)
