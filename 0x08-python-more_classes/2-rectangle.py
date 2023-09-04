b4162362b5a289ad252435c4535e72e87aba9304
walidlatif <walidlatif45@gmail.com>
2023-09-04 04:37:58 -0700

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -94,5 +94,4 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return 0
-        else:
-            return (self.width + self.height) * 2
+        return (self.width + self.height) * 2
