2ff41e61896501387b0abf1eff90847b025411b5
walidlatif <walidlatif45@gmail.com>
2023-09-12 09:30:26 -0700

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module : write_file '''
+
+
+def write_file(filename="", text=""):
+    """
+    Writes the specified text to the given file.
+
+    Args:
+         filename (str): The name of the file.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
