95b00709203797bceb66a08ea5491d03860e7e9a
walidlatif <walidlatif45@gmail.com>
2023-09-18 15:08:46 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,3 +20,40 @@ class Square(Rectangle):
         """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """
+        Getter
+        Returns:
+            Square size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+        Args:
+            value (int): rectangle size
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Function that assigns an argument to each attribute
+            1st argument should be the id attribute
+            2nd argument should be the width attribute
+            3rd argument should be the height attribute
+            4th argument should be the x attribute
+            5th argument should be the y attribute
+        """
+        attribute_names = ["id", "size", "x", "y"]
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, attribute_names[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if key in attribute_names:
+                    setattr(self, key, value)
