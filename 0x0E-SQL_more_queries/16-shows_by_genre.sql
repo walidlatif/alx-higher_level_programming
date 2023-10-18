c0222aaaf87d7381da88c99f355e7aac7d62a373
walidlatif <walidlatif45@gmail.com>
2023-10-18 12:03:09 -0700

    16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 15-comedy_only.sql)
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title, tv_genres.name;1~
