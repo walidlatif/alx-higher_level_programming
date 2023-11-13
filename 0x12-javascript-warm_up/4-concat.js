5e6e9174589abe52795543fa46c7c4ea0a1f1ea8
walidlatif <walidlatif45@gmail.com>
2023-11-13 10:20:06 -0800

    4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,6 @@
 if (!process.argv[2]) {
   process.argv[2] = 'undefined';
 } if (!process.argv[3]) {
-  process.argv[2] = 'undefined';
+  process.argv[3] = 'undefined';
 }
 console.log(process.argv[2], 'is', process.argv[3]);
