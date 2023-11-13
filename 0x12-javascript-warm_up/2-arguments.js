35d2c13f60dcbca8632655d8b61c13d9b3d62417
walidlatif <walidlatif45@gmail.com>
2023-11-13 09:13:42 -0800

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const { argv } = require('process');
-argv.length <= 2 ? console.log('No argument') : console.log('Argument found');
+argv.length <= 2 ? console.log('No argument') : argv.length >= 3 ? console.log('Argument found') : console.log('Argument found');
