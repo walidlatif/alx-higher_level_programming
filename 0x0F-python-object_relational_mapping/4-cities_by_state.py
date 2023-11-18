4cdc8111697bee88e4493578338e91172faa3ca9
walidlatif <walidlatif45@gmail.com>
2023-11-18 06:48:47 -0800

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+lists all states from the database
+'''
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+            )
+    cursor = connection.cursor()
+    query = f"SELECT * FROM cities " \
+            f"ORDER BY id ASC"
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    for d in data:
+        print(d)
+
+    cursor.close()
+    connection.close()
