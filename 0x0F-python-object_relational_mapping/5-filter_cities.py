e24e7038331453835b7b024ea7d995a292f833bc
walidlatif <walidlatif45@gmail.com>
2023-11-19 07:02:59 -0800

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     query = (
         "SELECT cities.name FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
+        "WHERE states.name LIKE BINARY %s "
         "ORDER BY cities.id ASC"
         )
     cursor.execute(query, (state_name,))
